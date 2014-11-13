f22fea508e751fbaa99e77bbdd93d483706b0925
James Simshaw <james.simshaw@gmail.com>
2014-11-12 20:08:14 -0800

    Added command line parsing, and some other cleanup

--- python/Fibboncci/fib.py
+++ python/Fibboncci/fib.py
@@ -1,10 +1,18 @@
+import argparse
+
 def fib(iterations):
     a = 1
     print("Fibboncci number 1 is {}.".format(a))
     b = 1
     print("Fibboncci number 2 is {}.".format(b))
-    for(i = 3; i < iterations; i++):
+    for i in range(3,iterations+1):
         c = a + b
         a = b
         b = c
         print("Fibboncci number {} is {}.".format(i, b))
+        
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("iterations", help="Number of iterations to run.", type=int)
+    args = parser.parse_args()
+    fib(args.iterations)
